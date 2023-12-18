485294557dccabe9a73a6828fa07486301833e5b
Yash Mittal <yashmittalmm@gmail.com>
2023-12-18 20:22:18 +0530

    added basic interactivity to the hero section swiper

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,21 +1,46 @@
+import { useState } from "react";
 import { RightArrow } from "./DynamicIcons";
 
 export default function HeroSection() {
+  const swiperImgUrls = [
+    "Images/heroBanner1.png",
+    "Images/heroBanner2.png",
+    "Images/heroBanner3.png",
+    "Images/heroBanner4.png",
+  ];
+
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+
+  function setPreviousImg() {
+    if (currentImgIndex <= 0) {
+      setCurrentImgIndex(swiperImgUrls.length - 1);
+    } else {
+      setCurrentImgIndex(currentImgIndex - 1);
+    }
+  }
+
+  function setNextImg() {
+    if (currentImgIndex >= swiperImgUrls.length - 1) {
+      setCurrentImgIndex(0);
+    } else {
+      setCurrentImgIndex(currentImgIndex + 1);
+    }
+  }
+
   return (
     <div className="w-full h-[66vh] relative">
-      {/* <img src="Images/heroBanner1.png" alt="" className="w-full h-full object-cover" /> */}
       <img
-        src="Images/bannerSale2.jpg"
+        src={swiperImgUrls[currentImgIndex]}
         alt=""
         className="w-full h-full object-cover object-top"
       />
       <div className="absolute top-1/2 left-0 right-0 flex px-3">
-        <button className="rounded-md p-3">
+        <button className="rounded-md p-3" onClick={setPreviousImg}>
           <span>
             <RightArrow classIcon={"rotate-180"} />
           </span>
         </button>
-        <button className="rounded-md p-3 ms-auto">
+        <button className="rounded-md p-3 ms-auto" onClick={setNextImg}>
           <RightArrow />
         </button>
       </div>
